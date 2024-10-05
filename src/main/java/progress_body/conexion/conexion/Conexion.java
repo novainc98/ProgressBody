3ca683a66d380395d918faadf4fb28d912228927
novainc98 <novainc98@gmail.com>
2024-10-05 12:33:05 -0600

    docs: Agregar comentarios y Javadocs a la clase Conexion
    Se han añadido Javadocs y comentarios en el código para mejorar la claridad
    sobre la función y el uso de la clase Conexion, facilitando la comprensión
    del código por parte de otros desarrolladores.

--- src/main/java/progress_body/conexion/conexion/Conexion.java
+++ src/main/java/progress_body/conexion/conexion/Conexion.java
@@ -4,20 +4,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Esta clase proporciona un método para establecer y gestionar la conexión a una base de datos MySQL.
+ */
 public class Conexion {
+    /**
+     * Establece una conexión a la base de datos MySQL.
+     *
+     * @return Connection - Un objeto de tipo Connection si se establece la conexión correctamente.
+     * @throws SQLException - Si no se puede establecer la conexión, ya sea por credenciales incorrectas o por el servidor apagado.
+     */
     public static Connection getConexion() throws SQLException {
-        String db = "progress_body_db";
-        String url = "jdbc:mysql://localhost:3306/" + db;
-        String user = "root";
-        String pass = "root1234";
+        String db = "progress_body_db"; // Nombre de la base de datos
+        String url = "jdbc:mysql://localhost:3306/" + db; // URL de conexión a la base de datos MySQL
+        String user = "root"; // Usuario de la base de datos
+        String pass = "root1234"; // Contraseña del usuario
 
         try {
+            // Se retorna un objeto de tipo Connection si la conexión se establece correctamente.
             return DriverManager.getConnection(url, user, pass);
         } catch (SQLException e) {
-            System.out.println("Error, al establecer la conexión a la base de datos MySql: " + e.getMessage());
-            throw new SQLException("Error, Las credenciales incorrectas o el servidor apagado.");
+            System.out.println("Error al establecer la conexión a la base de datos MySQL: " + e.getMessage());
+            throw new SQLException("Error: Credenciales incorrectas o el servidor está apagado.");
         }
     }
+
 //    public static void main(String[] args) {
 //        Connection con = null;
 //        try {
